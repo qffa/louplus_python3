558a2c8f4132916c81f33f31455c5e11125a4119
red292012 <red292012@163.com>
2017-10-18 11:25:42 +0800

    update challenge02

--- challenge02-tax-calc2/calculator.py
+++ challenge02-tax-calc2/calculator.py
@@ -2,60 +2,79 @@
 
 import sys
 
-try:
-	salary_value = int(sys.argv[1])
-except:
-	print("input invalid")
-	exit()
+l = []
+for i in range((len(sys.argv) - 1)):
+	l.append(tuple(sys.argv[i + 1].split(":")))
 
-social_insurance_rate = {"pension": 0.08, "medical":0.02, "unemployment":0.005, "house_founding":0.06, "birth":0, "injury":0}
+employee = dict(tuple(l))
 
-social_insurance_rate_sum = sum(list(social_insurance_rate.values()))
+print(employee)
 
 
-tax_begin_point = 3500
+def print_employee_tax(employee):
 
-
-salary_for_tax = salary_value * (1 - social_insurance_rate_sum) - tax_begin_point
-
-
-if salary_for_tax <= 0:
-	tax_rate = 0
-	tax_sub_value = 0
-
-elif salary_for_tax < 1500:
-	tax_rate = 0.03
-	tax_sub_value = 0
-
-elif salary_for_tax < 4500:
-	tax_rate = 0.1
-	tax_sub_value = 105
-
-elif salary_for_tax < 9000:
-	tax_rate = 0.2
-	tax_sub_value = 555
-
-elif salary_for_tax < 35000:
-	tax_rate = 0.25
-	tax_sub_value = 1005
-
-elif salary_for_tax < 55000:
-	tax_rate = 0.30
-	tax_sub_value = 2755
-
-elif salary_for_tax < 80000:
-	tax_rate = 0.35
-	tax_sub_value - 5505
-
-else:
-	tax_rate = 0.45
-	tax_sub_value = 13505
-
-
-tax_value = salary_for_tax * tax_rate - tax_sub_value
-
-
-print(format(tax_value,".2f"))
+	for key, value in employee.items():
+		try:
+			empid = int(key)
+			empsalary = int(value)
+		except:
+			print("Parameter Error")
+			exit()
+		
+		print(empid,format(tax(empsalary),".2f"))
 
 
 
+def tax(salary_value):
+	social_insurance_rate = {"pension": 0.08, "medical":0.02, "unemployment":0.005, "house_founding":0.06, "birth":0, "injury":0}
+
+	social_insurance_rate_sum = sum(list(social_insurance_rate.values()))
+
+
+	tax_begin_point = 3500
+
+
+	salary_for_tax = salary_value * (1 - social_insurance_rate_sum) - tax_begin_point
+
+
+	if salary_for_tax <= 0:
+		tax_rate = 0
+		tax_sub_value = 0
+
+	elif salary_for_tax < 1500:
+		tax_rate = 0.03
+		tax_sub_value = 0
+
+	elif salary_for_tax < 4500:
+		tax_rate = 0.1
+		tax_sub_value = 105
+
+	elif salary_for_tax < 9000:
+		tax_rate = 0.2
+		tax_sub_value = 555
+
+	elif salary_for_tax < 35000:
+		tax_rate = 0.25
+		tax_sub_value = 1005
+
+	elif salary_for_tax < 55000:
+		tax_rate = 0.30
+		tax_sub_value = 2755
+
+	elif salary_for_tax < 80000:
+		tax_rate = 0.35
+		tax_sub_value - 5505
+
+	else:
+		tax_rate = 0.45
+		tax_sub_value = 13505
+
+
+	return (salary_for_tax * tax_rate - tax_sub_value)
+
+
+#	print(format(tax_value,".2f"))
+
+
+
+print_employee_tax(employee)
