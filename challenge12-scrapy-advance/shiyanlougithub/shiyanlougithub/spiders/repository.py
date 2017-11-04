fd41fbe42d4a4aa95c7ac14bdf9a2b27d30d0611
red292012 <red292012@163.com>
2017-11-04 23:34:39 +0800

    pass

--- challenge12-scrapy-advance/shiyanlougithub/shiyanlougithub/spiders/repository.py
+++ challenge12-scrapy-advance/shiyanlougithub/shiyanlougithub/spiders/repository.py
@@ -24,7 +24,8 @@ class RepositorySpider(scrapy.Spider):
                 'name': repository.css('h3 a::text').re_first('[\s]*(.*)'),
                 'update_time': repository.css('relative-time::attr(datetime)').extract_first()
                 })
-            repository_url = response.urljoin(response.css('h3 a::attr(href)').extract_first())
+            repository_url = response.urljoin(repository.css('h3 a::attr(href)').extract_first())
+            print(repository_url)
             request = scrapy.Request(repository_url, callback = self.parse_repo)
             request.meta['item'] = item
 
@@ -39,5 +40,6 @@ class RepositorySpider(scrapy.Spider):
         item['commits'] = int(numbers[0])
         item['branches'] = int(numbers[1])
         item['releases'] = int(numbers[2])
+
 
         yield item
