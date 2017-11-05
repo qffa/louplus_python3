db3ae55c1080ce5fb1af5c1bdc617c395e06e82c
red292012 <red292012@163.com>
2017-11-05 12:06:26 +0800

    add loginspider

---
+++ lab21-scrapy-advance/shiyanlou/shiyanlou/spiders/login_spider.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class LoginSpider(scrapy.Spider):
+    name = 'loginspider'
+#    allowed_domains = ['shiyanlou.com/courses']
+    start_urls = ['https://www.shiyanlou.com/login']
+
+
+    def parse(self, response):
+        csrf_token = response.xpath('//div[@class="login-body"]//input[@id="csrf_token"]/@value').extract_first()
+        self.logger.info(csrf_token)
+        return scrapy.FormRequest.from_response(
+                response,
+                formdata={
+                    'csrf_token': csrf_token,
+                    'login': 'red292012@163.com',
+                    'password': 'xxx'
+                },
+                callback=self.after_login
+            )
+
+
+    def after_login(self, response):
+        return [scrapy.Request(url='https://www.shiyanlou.com/user/158680', callback=self.parse_after_login)]
+
+
+    def parse_after_login(self, response):
+        return {
+                'lab_count': response.xpath('(//span[@class="info-text"])[2]/text()').re_first('[^\d]*(\d*)'),
+                'lab_minutes': response.xpath('(//span[@class="info-text"])[3]/text()').re_first('[^\d]*(\d*)[^\d*]')
+            }
+
+
+
