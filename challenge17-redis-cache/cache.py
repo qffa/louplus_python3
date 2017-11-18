45db04d16367ef8bc91ea7faa5540e38f998cb5f
red292012 <red292012@163.com>
2017-11-18 19:56:05 +0800

    update ch17

--- challenge17-redis-cache/cache.py
+++ challenge17-redis-cache/cache.py
@@ -2,9 +2,6 @@ import redis
 import json
 
 
-r = redis.StrictRedis(host='127.0.0.1', db=0)
-
-
 class RedisCache:
     def __init__(self, redis_client):
         self._redis = redis_client
@@ -12,15 +9,17 @@ class RedisCache:
     def cache(self, timeout=0):
         def deco(func):
             def wrapper(*args, **kwargs):
-                key = (args, kwargs)
+                skwargs = sorted(kwargs.items())
+                key = (args, skwargs)
                 if self._redis.exists(key):
                     data = self._redis.get(key)
+                    print('from cache')
                     return json.loads(data.decode())
-
                 else:
                     data = func(*args, **kwargs)
-                    data_json = json.dumps(data)
-                    self._redis.set(key, data_json)
+                    assert isinstance(data, dict), "%s doesn't return dictiionary" %func
+                    jdata = json.dumps(data)
+                    self._redis.set(key, jdata)
                     self._redis.expire(key, timeout)
 
                     return data
@@ -30,18 +29,18 @@ class RedisCache:
 
 
 
-
-
-
+r = redis.StrictRedis(host='127.0.0.1', db=0)
 cache = RedisCache(r)
+
 @cache.cache(timeout=10)
-def execute(n):
+def execute(n, a, b, c):
     
     return {'n': n, 'n2': n * 2}
+#    return 0
 
 
 if __name__ == '__main__':
-    print(execute('10'))
+    print(execute('10', a = 1, b = 2, c = 3))
 
 
 
