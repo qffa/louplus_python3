059d2e039cad1c830fa164cbc6108c6870464730
red292012 <red292012@163.com>
2017-12-02 17:58:05 +0800

    init lab41

---
+++ lab41-simpledu-admin/simpledu/simpledu/handlers/admin.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, current_app
+from simpledu.decorators import admin_required
+from simpledu.models import Course
+
+
+admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+
+@admin.route('/')
+@admin_required
+def index():
+    return render_template('admin/index.html')
+
+
+
+@admin.route('/courses')
+@admin_required
+def courses():
+    page = request.args.get('page', default=1, type=int)
+    pagination = Course.query.paginate(
+            page = page,
+            per_page = current_app.config['ADMIN_PER_PAGE'],
+            error_out = False
+            )
+
+    return render_template('admin/courses.html', pagination=pagination)
+
+
+
+
+
+@admin.route('/create_course')
+@admin_required
+def create_course():
+    pass
+
