2ee71063880ffde2ec3f63356d7ff4aba7029a0b
red292012 <red292012@163.com>
2017-12-05 09:49:00 +0800

    lab43 init

---
+++ lab43-simpedu-live/simpledu/simpledu/app.py
@@ -0,0 +1,59 @@
+from flask import Flask 
+from simpledu.config import configs
+from simpledu.models import db, Course, User
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+
+
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
+
+
+
+
+
+
+def register_blueprint(app):
+    from .handlers import front, course, admin, user, live
+
+    app.register_blueprint(front)
+    app.register_blueprint(course)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+    app.register_blueprint(live)
+
+    
+
+
+
+def create_app(config):
+    """ APP 工厂
+
+    """
+
+    app = Flask(__name__)
+    app.config.from_object(configs.get(config))
+
+    register_extensions(app)
+    register_blueprint(app)
+
+
+
+    return app
+
+
+
+
+
